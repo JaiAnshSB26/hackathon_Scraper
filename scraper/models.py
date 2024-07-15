ab58f36a7f07efc9c45758fd2dad11b144b5c2c2
Jai Ansh Singh Bindra <118647945+JaiAnshSB26@users.noreply.github.com>
2024-07-15 18:37:55 +0200

    Update models.py

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Hackathon(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    location = models.CharField(max_length=200)
+    website = models.URLField()
+
+    def __str__(self):
+        return self.title
+
